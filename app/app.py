e7969fb34e28078c07bc0951beaac2f03a9d966b
Clemens <cleemansen@gmail.com>
2024-03-11 14:46:24 +0100

    Initialize feature/user_file-app-setting

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from sdk.moveapps_spec import hook_impl
+from sdk.moveapps_io import MoveAppsIo
+from movingpandas import TrajectoryCollection
+import logging
+
+
+class App(object):
+
+    def __init__(self, moveapps_io):
+        self.moveapps_io = moveapps_io
+
+    @hook_impl
+    def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
+        """Your app code goes here"""
+        logging.info(f'Welcome to the {config}')
+
+        auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
+        with open(auxiliary_file_a, 'r') as f:
+            logging.info(f.read())
+
+        # return some useful data for next apps in the workflow
+        return data
